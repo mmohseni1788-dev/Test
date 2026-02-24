a567ca6928031aaa994295834181be4164fc0ef6
M-MOHSENI <M-MOHSENI@agri-bank.com>
2026-02-24 15:40:31 +0330

    Add project files.

---
+++ Test/AuthorizationEngine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Test
+{
+    public static class AuthorizationEngine
+    {
+        public static void Invoke(object target, string methodName,UserContext user)
+        {
+            var method = target.GetType().GetMethod(methodName);
+            if (method == null) throw new Exception("Method not found!");
+
+            var authAttr = method.GetCustomAttribute<AuthorizationAttribute>();
+            if (authAttr != null)
+            {
+                if (user.Role != authAttr.Role)
+                {
+                    throw new UnauthorizedAccessException($"Access denied. Required role: {authAttr.Role}");
+                }
+            }
+            method.Invoke(target, null);
+        }
+    }
+}
---
+++ Test/UserContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    public sealed class UserContext
+    {
+        public string UserName { get; }
+        public string Role { get;}
+        public UserContext(string userName , string role)
+        {
+            UserName = userName;
+            Role = role;
+        }
+    }
+}
